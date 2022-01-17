9509a43282940871f50f58b519b190c7366878bd
Vishnu C <vishnuc@Vishnus-MacBook-Air.local>
2022-01-17 23:29:59 +0530

    ZKX-93 adds authentication to fee mapping updation

---
+++ contracts/CallFeeBalance.cairo
@@ -0,0 +1,47 @@
+%lang starknet
+
+%builtins pedersen range_check ecdsa
+
+from starkware.cairo.common.cairo_builtins import HashBuiltin
+from starkware.cairo.common.math import assert_not_equal
+from starkware.starknet.common.syscalls import get_caller_address
+
+# @notice Stores the address of FeeBalance contract
+@storage_var
+func fee_address() -> (contract_address : felt):
+end
+
+# @notice Constructor of the smart-contract
+# @param _authAddress Address of the FeeBalance contract
+@constructor
+func constructor{
+    syscall_ptr : felt*, 
+    pedersen_ptr : HashBuiltin*, 
+    range_check_ptr
+}(_authAddress : felt):
+
+    fee_address.write(value = _authAddress)
+    return ()
+end
+
+# @notice Function to call update_fee_mapping of FeeBalance contract
+# @param _address - address of the user whose fee is to be updated
+# @param _fee_to_add - fee that is to be added
+@external
+func update{
+    syscall_ptr : felt*, 
+    pedersen_ptr : HashBuiltin*,
+    range_check_ptr
+}(_address: felt, _fee_to_add: felt) :
+    alloc_locals
+    let (fee_addr) = fee_address.read()
+    IFeeBalance.update_fee_mapping(contract_address = fee_addr, address = _address, fee_to_add = _fee_to_add)
+    return()
+end
+
+# @notice FeeBalance interface
+@contract_interface
+namespace IFeeBalance:
+    func update_fee_mapping(address : felt, fee_to_add : felt):
+    end
+end
--- tests/test_FeeBalance.py
+++ tests/test_FeeBalance.py
@@ -5,13 +5,15 @@ from starkware.starkware_utils.error_handling import StarkException
 from starkware.starknet.definitions.error_codes import StarknetErrorCode
 from utils import Signer, uint, str_to_felt, MAX_UINT256, assert_revert
 
-signer = Signer(123456789987654321)
-signer1 = Signer(123456789987654322)
-signer2 = Signer(123456789987654323)
+signer1 = Signer(123456789987654321)
+signer2 = Signer(123456789987654322)
+signer3 = Signer(123456789987654323)
+signer4 = Signer(123456789987654324)
 
 @pytest.fixture
 def global_var():
     pytest.user1 = None
+    pytest.user2 = None
 
 @pytest.fixture(scope='module')
 def event_loop():
@@ -21,33 +23,53 @@ def event_loop():
 @pytest.fixture(scope='module')
 async def feeBalance_factory():
     starknet = await Starknet.empty()
-    admin = await starknet.deploy(
-        "contracts/Account.cairo",
-        constructor_calldata=[signer.public_key]
-    )
-
-    pytest.user1 = await starknet.deploy(
+    admin1 = await starknet.deploy(
         "contracts/Account.cairo",
         constructor_calldata=[signer1.public_key]
     )
 
-    pytest.user2 = await starknet.deploy(
+    admin2 = await starknet.deploy(
         "contracts/Account.cairo",
         constructor_calldata=[signer2.public_key]
     )
 
-    feeBalance = await starknet.deploy(
-        "contracts/FeeBalance.cairo",
-        constructor_calldata=[]
+    pytest.user1 = await starknet.deploy(
+        "contracts/Account.cairo",
+        constructor_calldata=[signer3.public_key]
     )
 
-    return feeBalance, admin
+    pytest.user2 = await starknet.deploy(
+        "contracts/Account.cairo",
+        constructor_calldata=[signer4.public_key]
+    )
+
+    adminAuth = await starknet.deploy(
+        "contracts/AdminAuth.cairo",
+        constructor_calldata=[
+            admin1.contract_address,
+            admin2.contract_address
+        ]
+    )
+
+    feeBalance = await starknet.deploy(
+        "contracts/FeeBalance.cairo",
+        constructor_calldata=[adminAuth.contract_address]
+    )
+
+    callFeeBalance = await starknet.deploy(
+        "contracts/CallFeeBalance.cairo",
+        constructor_calldata=[feeBalance.contract_address]
+    )
+
+    return feeBalance, callFeeBalance, admin1, admin2
 
 @pytest.mark.asyncio
 async def test_update_fee_mapping(feeBalance_factory):
-    feeBalance, admin = feeBalance_factory
+    feeBalance, callFeeBalance, admin1, _ = feeBalance_factory
 
-    await signer.send_transaction(admin, feeBalance.contract_address, 'update_fee_mapping', [pytest.user1.contract_address, 10])
+    await signer1.send_transaction(admin1, feeBalance.contract_address, 'update_caller_address', [callFeeBalance.contract_address])
+
+    await signer1.send_transaction(admin1, callFeeBalance.contract_address, 'update', [pytest.user1.contract_address, 10])
 
     execution_info = await feeBalance.get_total_fee().call()
     assert execution_info.result.fee == 10
@@ -57,9 +79,9 @@ async def test_update_fee_mapping(feeBalance_factory):
 
 @pytest.mark.asyncio
 async def test_update_fee_mapping_different_user(feeBalance_factory):
-    feeBalance, admin = feeBalance_factory
+    feeBalance, callFeeBalance, admin1, _ = feeBalance_factory
 
-    await signer.send_transaction(admin, feeBalance.contract_address, 'update_fee_mapping', [pytest.user1.contract_address, 10])
+    await signer1.send_transaction(admin1, callFeeBalance.contract_address, 'update', [pytest.user1.contract_address, 10])
 
     execution_info = await feeBalance.get_total_fee().call()
     assert execution_info.result.fee == 20
@@ -67,7 +89,7 @@ async def test_update_fee_mapping_different_user(feeBalance_factory):
     execution_info = await feeBalance.get_user_fee(pytest.user1.contract_address).call()
     assert execution_info.result.fee == 20
 
-    await signer.send_transaction(admin, feeBalance.contract_address, 'update_fee_mapping', [pytest.user2.contract_address, 10])
+    await signer1.send_transaction(admin1, callFeeBalance.contract_address, 'update', [pytest.user2.contract_address, 10])
 
     execution_info = await feeBalance.get_total_fee().call()
     assert execution_info.result.fee == 30
