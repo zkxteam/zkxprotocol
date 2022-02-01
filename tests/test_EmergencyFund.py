ae4c342f1e1e52dbe97f3351337ec902e731bbf1
Temirzhan Yussupov <timka2609@gmail.com>
2022-02-01 11:11:03 +0600

    ZKX-114 implement smart contract and tests

---
+++ tests/test_EmergencyFund.py
@@ -0,0 +1,81 @@
+import pytest
+import asyncio
+from starkware.starknet.testing.starknet import Starknet
+from starkware.starkware_utils.error_handling import StarkException
+from starkware.starknet.definitions.error_codes import StarknetErrorCode
+from utils import Signer, uint, str_to_felt, MAX_UINT256, assert_revert
+
+signer1 = Signer(123456789987654321)
+signer2 = Signer(123456789987654322)
+signer3 = Signer(123456789987654323)
+signer4 = Signer(123456789987654324)
+
+
+@pytest.fixture
+def global_var():
+    pytest.user1 = None
+    pytest.user2 = None
+
+
+@pytest.fixture(scope='module')
+def event_loop():
+    return asyncio.new_event_loop()
+
+
+@pytest.fixture(scope='module')
+async def emergencyFund_factory():
+    starknet = await Starknet.empty()
+    admin1 = await starknet.deploy(
+        "contracts/Account.cairo",
+        constructor_calldata=[signer1.public_key, 0]
+    )
+
+    admin2 = await starknet.deploy(
+        "contracts/Account.cairo",
+        constructor_calldata=[signer2.public_key, 0]
+    )
+
+    pytest.user1 = await starknet.deploy(
+        "contracts/Account.cairo",
+        constructor_calldata=[signer3.public_key, 0]
+    )
+
+    pytest.user2 = await starknet.deploy(
+        "contracts/Account.cairo",
+        constructor_calldata=[signer4.public_key, 0]
+    )
+
+    adminAuth = await starknet.deploy(
+        "contracts/AdminAuth.cairo",
+        constructor_calldata=[
+            admin1.contract_address,
+            admin2.contract_address
+        ]
+    )
+
+    emergencyFund = await starknet.deploy(
+        "contracts/EmergencyFund.cairo",
+        constructor_calldata=[adminAuth.contract_address, 0]
+    )
+
+    return emergencyFund, admin1, admin2
+
+
+@pytest.mark.asyncio
+async def test_fund_invalid(emergencyFund_factory):
+    emergencyFund, admin1, admin2 = emergencyFund_factory
+    assert_revert(lambda: signer3.send_transaction(
+        pytest.user1, emergencyFund.contract_address, 'fund', [str_to_felt("TSLA"), 0]))
+
+
+@pytest.mark.asyncio
+async def test_funding_flow(emergencyFund_factory):
+    emergencyFund, admin1, admin2 = emergencyFund_factory
+
+    await signer1.send_transaction(admin1, emergencyFund.contract_address, 'fund', [str_to_felt("TSLA"), 10])
+    execution_info = await emergencyFund.balance(str_to_felt("TSLA")).call()
+    assert execution_info.result.amount == 10
+
+    await signer2.send_transaction(admin2, emergencyFund.contract_address, 'defund', [str_to_felt("TSLA"), 3])
+    execution_info = await emergencyFund.balance(str_to_felt("TSLA")).call()
+    assert execution_info.result.amount == 7
