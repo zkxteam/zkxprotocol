1bc741087edf3879ad40fc1888d3866c3d317d14
udayzkx <uday@zkx.fi>
2022-08-03 18:18:25 +0000

    updates tests for admin auth, formatting

--- L2/contracts/AdminAuth.cairo
+++ L2/contracts/AdminAuth.cairo
@@ -8,7 +8,7 @@ from starkware.cairo.common.math import assert_not_equal, assert_lt, assert_nn, 
 from starkware.cairo.common.bool import TRUE, FALSE
 from starkware.starknet.common.syscalls import get_caller_address
 
-#### STORAGE VARS ####
+# ### STORAGE VARS ####
 # @notice Stores the access permissions
 # Action - role
 # 0 - Master admin
@@ -22,38 +22,43 @@ from starkware.starknet.common.syscalls import get_caller_address
 func admin_mapping(address : felt, action : felt) -> (allowed : felt):
 end
 
+# stores the minimum number of admins required to be in the system
+# this number can be > current total admins in the system since this number is only used to check whether
+# admin removal is possible
 @storage_var
-func min_num_admins() -> (res: felt):
+func min_num_admins() -> (res : felt):
 end
 
+# stores previous approver for an address (used to update MasterAdmin_ACTION)
 @storage_var
-func approver(address: felt) -> (approver_address: felt):
+func approver(address : felt) -> (approver_address : felt):
 end
 
+# stores previous remover (the admin which proposed removal) for an address (used to update MasterAdmin_ACTION)
 @storage_var
-func remover(address: felt) -> (remover_address: felt):
+func remover(address : felt) -> (remover_address : felt):
 end
 
+# stores the number of admins currently in the system
 @storage_var
-func current_total_admins() -> (res: felt):
+func current_total_admins() -> (res : felt):
 end
-
 
 # @notice Constructor for the contract
 # @param address1 - Address for first initial admin
 # @param address2 - Address for second initial admin
 @constructor
 func constructor{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(
-    address1 : felt, address2 : felt
-):
+        address1 : felt, address2 : felt):
     # Action 0 is the admin role - the role which can add and remove admins
     admin_mapping.write(address=address1, action=MasterAdmin_ACTION, value=1)
     admin_mapping.write(address=address2, action=MasterAdmin_ACTION, value=1)
     min_num_admins.write(2)
+    current_total_admins.write(2)
     return ()
 end
 
-#### VIEW FUNCTIONS ####
+# ### VIEW FUNCTIONS ####
 
 # @notice Function to find whether an address has permission to perform a specific role
 # @param address - Address for which permission has to be determined
@@ -61,30 +66,34 @@ end
 # @return allowed - 0 if no access, 1 if access allowed
 @view
 func get_admin_mapping{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(
-    address : felt, action : felt
-) -> (allowed : felt):
+        address : felt, action : felt) -> (allowed : felt):
     let (allowed) = admin_mapping.read(address=address, action=action)
     return (allowed=allowed)
 end
 
 @view
-func get_min_num_admins{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(
-) -> (res: felt):
-
+func get_min_num_admins{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}() -> (
+        res : felt):
     let (res) = min_num_admins.read()
-    return(res)
+    return (res)
 end
 
-#### EXTERNAL FUNCTIONS ####
+@view
+func get_current_total_admins{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(
+        ) -> (res : felt):
+    let (res) = current_total_admins.read()
+    return (res)
+end
+
+# ### EXTERNAL FUNCTIONS ####
 
 # @notice Function to update the permissions
 # @param address - Address for which permissions are to be granted/revoked
 # @param action - Role that needs to be granted/revoked
-# @param value - 0 for revoking, 1 for granting
+# @param value - 0 (FALSE) for revoking, 1 (TRUE) for granting
 @external
 func update_admin_mapping{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(
-    address : felt, action : felt, value : felt
-):
+        address : felt, action : felt, value : felt):
     # Verify that caller has admin role
     let (caller) = get_caller_address()
     let (is_admin) = admin_mapping.read(address=caller, action=MasterAdmin_ACTION)
@@ -93,99 +102,97 @@ func update_admin_mapping{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, rang
     # if current permission for <address, action> is same as value then simply return without any processing
     let (current_val) = admin_mapping.read(address=address, action=action)
     if current_val == value:
-        return()
+        return ()
     end
 
     # arg value should either be 0 or 1
     with_attr error_message("Permission can only be 0 or 1"):
-        let val_check = value * (1-value)
+        let val_check = value * (1 - value)
         assert val_check = FALSE
     end
 
     # Add or remove admin action
     if action == MasterAdmin_ACTION:
-
         process_admin_action(address, value, caller)
-        return()
+        return ()
         # All other actions
     else:
         admin_mapping.write(address=address, action=action, value=value)
-        return()
+        return ()
     end
-    
 end
 
+# @notice - Callable only by admin, this function sets the minimum number of admins that should be present in the system
+# @param num - number of min admins to set (must be >= 2)
 @external
 func set_min_num_admins{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(
-    num: felt
-):
-
+        num : felt):
     # Verify that caller has admin role
     let (caller) = get_caller_address()
     let (is_admin) = admin_mapping.read(address=caller, action=MasterAdmin_ACTION)
     assert is_admin = TRUE
 
     assert_nn(num)
-    assert_le(2, num) # we enforce that there have to be atleast 2 admins in the system
+    assert_le(2, num)  # we enforce that there have to be atleast 2 admins in the system
     min_num_admins.write(num)
-    return()
+    return ()
 end
 
+# ### INTERNAL FUNCTIONS ####
 
-
-#### INTERNAL FUNCTIONS ####
+# @notice - This function processes the update admin mapping call when action = MasterAdmin_ACTION
+# It checks that caller is not the same as previous approver/remover and updates the mapping accordingly
 func process_admin_action{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(
-    address: felt, value: felt, caller: felt
-):
+        address : felt, value : felt, caller : felt):
     if value == TRUE:
         # check that approver is not same as caller
         # if present approver is 0 then caller is first approver
         # save caller address as approver & return
         let (current_approver) = approver.read(address)
-        assert_not_equal(current_approver, caller)
+        with_attr error_message("Both approvers cannot be the same"):
+            assert_not_equal(current_approver, caller)
+        end
         if current_approver == 0:
             approver.write(address, caller)
-            return()
+            return ()
         else:
             # if present approver != 0, then this is 2nd approval
             # give admin permission to address and mark approver address as 0 for future approvals
             admin_mapping.write(address, MasterAdmin_ACTION, TRUE)
-            approver.write(address, 0) # this ensures that if address gets removed as admin, it would again need 2 approvals
+            approver.write(address, 0)  # this ensures that if address gets removed as admin, it would again need 2 approvals
             let (current_num_admins) = current_total_admins.read()
-            current_total_admins.write(current_num_admins+1) # increment number of admins in the system
-            return()
+            current_total_admins.write(current_num_admins + 1)  # increment number of admins in the system
+            return ()
         end
     else:
-
         # verify that we have more than minimum number of admins in the system
         # proceed with removal process only if condition is satisfied
         let (minimum_admins_required) = min_num_admins.read()
         let (current_num_admins) = current_total_admins.read()
-        assert_lt(minimum_admins_required, current_num_admins)
-    
+
+        with_attr error_message("Cannot have less than minimum number of admins"):
+            assert_lt(minimum_admins_required, current_num_admins)
+        end
+
         # check that remover is not the same as caller
         let (current_remover) = remover.read(address)
-        assert_not_equal(current_remover, caller)
+        with_attr error_message("Both removers cannot be the same"):
+            assert_not_equal(current_remover, caller)
+        end
         if current_remover == 0:
-            # if present remover is 0 then  caller is first remover
+            # if present remover is 0 then caller is first remover
             # save caller address as remover & return
             remover.write(address, caller)
-            return()
+            return ()
         else:
-            # if presentremover !=0, then this is 2nd removal call
+            # if present remover !=0, then this is 2nd removal call
             # revoke admin permission for address and mark remover as 0 for future removals
             admin_mapping.write(address, MasterAdmin_ACTION, FALSE)
             # this ensures that if address is added as admin, it would need 2 removals to revoke admin permission
-            remover.write(address, 0) 
+            remover.write(address, 0)
             let (current_num_admins) = current_total_admins.read()
-            current_total_admins.write(current_num_admins-1) # decrement number of admins in the system
-            return()
+            current_total_admins.write(current_num_admins - 1)  # decrement number of admins in the system
+            return ()
         end
     end
 end
-
-
-
-
-
-
--- L2/tests/test_AdminAuth.py
+++ L2/tests/test_AdminAuth.py
@@ -8,17 +8,11 @@ from utils import Signer, uint, str_to_felt, MAX_UINT256, assert_revert
 signer1 = Signer(123456789987654321)
 signer2 = Signer(123456789987654322)
 signer3 = Signer(123456789987654323)
-signer4 = Signer(123456789987654323)
-signer5 = Signer(123456789987654324)
+signer4 = Signer(123456789987654324)
+signer5 = Signer(123456789987654325)
 
 L1_dummy_address = 0x01234567899876543210
 L1_ZKX_dummy_address = 0x98765432100123456789
-
-@pytest.fixture
-def global_var():
-    pytest.user1 = None
-    pytest.user2 = None
-    pytest.user3 = None
 
 
 @pytest.fixture(scope='module')
@@ -31,27 +25,32 @@ async def adminAuth_factory():
     starknet = await Starknet.empty()
     admin1 = await starknet.deploy(
         "contracts/Account.cairo",
-        constructor_calldata=[signer1.public_key, L1_dummy_address, 0, 1, L1_ZKX_dummy_address]
+        constructor_calldata=[signer1.public_key,
+                              L1_dummy_address, 0, 1, L1_ZKX_dummy_address]
     )
 
     admin2 = await starknet.deploy(
         "contracts/Account.cairo",
-        constructor_calldata=[signer2.public_key, L1_dummy_address, 0, 1, L1_ZKX_dummy_address]
+        constructor_calldata=[signer2.public_key,
+                              L1_dummy_address, 0, 1, L1_ZKX_dummy_address]
     )
 
-    pytest.user1 = await starknet.deploy(
+    user1 = await starknet.deploy(
         "contracts/Account.cairo",
-        constructor_calldata=[signer3.public_key, L1_dummy_address, 0, 1, L1_ZKX_dummy_address]
+        constructor_calldata=[signer3.public_key,
+                              L1_dummy_address, 0, 1, L1_ZKX_dummy_address]
     )
 
-    pytest.user2 = await starknet.deploy(
+    user2 = await starknet.deploy(
         "contracts/Account.cairo",
-        constructor_calldata=[signer4.public_key, L1_dummy_address, 0, 1, L1_ZKX_dummy_address]
+        constructor_calldata=[signer4.public_key,
+                              L1_dummy_address, 0, 1, L1_ZKX_dummy_address]
     )
 
-    pytest.user3 = await starknet.deploy(
+    user3 = await starknet.deploy(
         "contracts/Account.cairo",
-        constructor_calldata=[signer5.public_key, L1_dummy_address, 0, 1, L1_ZKX_dummy_address]
+        constructor_calldata=[signer5.public_key,
+                              L1_dummy_address, 0, 1, L1_ZKX_dummy_address]
     )
 
     adminAuth = await starknet.deploy(
@@ -62,89 +61,201 @@ async def adminAuth_factory():
         ]
     )
 
-    return adminAuth, admin1, admin2
+    return adminAuth, admin1, admin2, user1, user2, user3
 
 
 @pytest.mark.asyncio
 async def test_get_admin_mapping(adminAuth_factory):
-    adminAuth, admin1, admin2 = adminAuth_factory
+    adminAuth, admin1, admin2, user1, user2, user3 = adminAuth_factory
 
     execution_info = await adminAuth.get_admin_mapping(admin1.contract_address, 0).call()
     assert execution_info.result.allowed == 1
 
-    execution_info1 = await adminAuth.get_admin_mapping(admin2.contract_address, 0).call()
-    assert execution_info1.result.allowed == 1
+    execution_info1 = await adminAuth.get_admin_mapping(user1.contract_address, 0).call()
+    assert execution_info1.result.allowed == 0
+
+
+@pytest.mark.asyncio
+async def test_get_min_num_admins(adminAuth_factory):
+    adminAuth, admin1, admin2, user1, user2, user3 = adminAuth_factory
+    result = await adminAuth.get_min_num_admins().call()
+    assert result.result.res == 2
+
+
+@pytest.mark.asyncio
+async def test_set_min_num_admins(adminAuth_factory):
+    adminAuth, admin1, admin2, user1, user2, user3 = adminAuth_factory
+    # cannot set negative number for min admins
+    await assert_revert(signer1.send_transaction(admin1, adminAuth.contract_address, 'set_min_num_admins', [-1]))
+    # cannot set any number less than 2
+    await assert_revert(signer1.send_transaction(admin1, adminAuth.contract_address, 'set_min_num_admins', [1]))
+    # only admin can call this function
+    await assert_revert(signer3.send_transaction(user1, adminAuth.contract_address, 'set_min_num_admins', [2]))
+
+    await signer1.send_transaction(admin1, adminAuth.contract_address, 'set_min_num_admins', [3])
+
+    result = await adminAuth.get_min_num_admins().call()
+    assert result.result.res == 3
+
+    await signer1.send_transaction(admin1, adminAuth.contract_address, 'set_min_num_admins', [2])
+
+    result = await adminAuth.get_min_num_admins().call()
+    assert result.result.res == 2
 
 
 @pytest.mark.asyncio
 async def test_update_admin_mapping_non_admin(adminAuth_factory):
-    adminAuth, admin1, _ = adminAuth_factory
+    adminAuth, admin1, admin2, user1, user2, user3 = adminAuth_factory
 
-    await signer1.send_transaction(admin1, adminAuth.contract_address, 'update_admin_mapping', [pytest.user1.contract_address, 1, 1])
+    await signer1.send_transaction(admin1, adminAuth.contract_address, 'update_admin_mapping', [user1.contract_address, 1, 1])
 
-    execution_info = await adminAuth.get_admin_mapping(pytest.user1.contract_address, 1).call()
+    execution_info = await adminAuth.get_admin_mapping(user1.contract_address, 1).call()
     assert execution_info.result.allowed == 1
 
+    # setting permission which is same as existing permission should also work - no processing done internally
+    await signer1.send_transaction(admin1, adminAuth.contract_address, 'update_admin_mapping', [user1.contract_address, 1, 1])
 
-@pytest.mark.asyncio
-async def test_update_admin_mapping_one_approval(adminAuth_factory):
-    adminAuth, admin1, _ = adminAuth_factory
+    execution_info = await adminAuth.get_admin_mapping(user1.contract_address, 1).call()
+    assert execution_info.result.allowed == 1
 
-    await signer1.send_transaction(admin1, adminAuth.contract_address, 'update_admin_mapping', [pytest.user2.contract_address, 0, 1])
-
-    execution_info1 = await adminAuth.get_admin_mapping(pytest.user2.contract_address, 0).call()
-    assert execution_info1.result.allowed == admin1.contract_address
-
-    assert_revert(lambda: signer4.send_transaction(pytest.user2, adminAuth.contract_address,
-                  'update_admin_mapping', [pytest.user2.contract_address, 0, 1]))
+    # user1 should not have been granted any other permission
+    execution_info = await adminAuth.get_admin_mapping(user1.contract_address, 0).call()
+    assert execution_info.result.allowed == 0
 
 
 @pytest.mark.asyncio
 async def test_update_admin_mapping_same_approval(adminAuth_factory):
-    adminAuth, admin1, _ = adminAuth_factory
+    adminAuth, admin1, admin2, user1, user2, user3 = adminAuth_factory
 
-    await signer1.send_transaction(admin1, adminAuth.contract_address, 'update_admin_mapping', [pytest.user3.contract_address, 0, 1])
+    await signer1.send_transaction(admin1, adminAuth.contract_address, 'update_admin_mapping', [user2.contract_address, 0, 1])
 
-    execution_info1 = await adminAuth.get_admin_mapping(pytest.user2.contract_address, 0).call()
-    assert execution_info1.result.allowed == admin1.contract_address
+    execution_info1 = await adminAuth.get_admin_mapping(user2.contract_address, 0).call()
+    assert execution_info1.result.allowed == 0
 
-    assert_revert(lambda: signer1.send_transaction(pytest.user2, adminAuth.contract_address,
-                  'update_admin_mapping', [pytest.user3.contract_address, 0, 1]))
+    # if same admin gives another approval then transaction will revert
+    await assert_revert(signer1.send_transaction(admin1, adminAuth.contract_address,
+                                                 'update_admin_mapping', [user2.contract_address, 0, 1]))
+
+    execution_info1 = await adminAuth.get_admin_mapping(user2.contract_address, 0).call()
+    assert execution_info1.result.allowed == 0
 
 
 @pytest.mark.asyncio
 async def test_update_admin_mapping_no_permission(adminAuth_factory):
-    adminAuth, _, _ = adminAuth_factory
+    adminAuth, admin1, admin2, user1, user2, user3 = adminAuth_factory
 
-    assert_revert(lambda: signer3.send_transaction(pytest.user1, adminAuth.contract_address,
-                  'update_admin_mapping', [pytest.user1.contract_address, 0, 1]))
+    # non-admins cannot update admin mapping
+    await assert_revert(signer3.send_transaction(user1, adminAuth.contract_address,
+                                                 'update_admin_mapping', [user1.contract_address, 0, 1]))
 
 
 @pytest.mark.asyncio
-async def test_update_admin_mapping_admin(adminAuth_factory):
-    adminAuth, admin1, admin2 = adminAuth_factory
+async def test_update_admin_mapping_incorrect_value(adminAuth_factory):
+    adminAuth, admin1, admin2, user1, user2, user3 = adminAuth_factory
 
-    await signer1.send_transaction(admin1, adminAuth.contract_address, 'update_admin_mapping', [pytest.user1.contract_address, 0, 1])
+    # only value of 0/1 can be used as value for permission
+    await assert_revert(signer1.send_transaction(admin1, adminAuth.contract_address,
+                                                 'update_admin_mapping', [user1.contract_address, 2, 2]))
 
-    execution_info1 = await adminAuth.get_admin_mapping(pytest.user1.contract_address, 0).call()
-    assert execution_info1.result.allowed == admin1.contract_address
 
-    await signer2.send_transaction(admin2, adminAuth.contract_address, 'update_admin_mapping', [pytest.user1.contract_address, 0, 1])
+@pytest.mark.asyncio
+async def test_add_admin_mapping_admin(adminAuth_factory):
+    adminAuth, admin1, admin2, user1, user2, user3 = adminAuth_factory
 
-    execution_info2 = await adminAuth.get_admin_mapping(pytest.user1.contract_address, 0).call()
+    result = await adminAuth.get_current_total_admins().call()
+    assert result.result.res == 2
+    await signer1.send_transaction(admin1, adminAuth.contract_address, 'update_admin_mapping', [user1.contract_address, 0, 1])
+
+    execution_info1 = await adminAuth.get_admin_mapping(user1.contract_address, 0).call()
+    assert execution_info1.result.allowed == 0
+
+    result = await adminAuth.get_current_total_admins().call()
+    assert result.result.res == 2
+
+    await signer2.send_transaction(admin2, adminAuth.contract_address, 'update_admin_mapping', [user1.contract_address, 0, 1])
+
+    execution_info2 = await adminAuth.get_admin_mapping(user1.contract_address, 0).call()
     assert execution_info2.result.allowed == 1
+
+    result = await adminAuth.get_current_total_admins().call()
+    assert result.result.res == 3
+
+
+@pytest.mark.asyncio
+async def test_remove_admin_mapping_admin(adminAuth_factory):
+    adminAuth, admin1, admin2, user1, user2, user3 = adminAuth_factory
+
+    result = await adminAuth.get_current_total_admins().call()
+    assert result.result.res == 3
+    await signer1.send_transaction(admin1, adminAuth.contract_address, 'update_admin_mapping', [user1.contract_address, 0, 0])
+
+    execution_info1 = await adminAuth.get_admin_mapping(user1.contract_address, 0).call()
+    assert execution_info1.result.allowed == 1
+
+    result = await adminAuth.get_current_total_admins().call()
+    assert result.result.res == 3
+
+    # if same admin gives 2 calls for removal then transaction will revert
+    await assert_revert(signer1.send_transaction(admin1, adminAuth.contract_address,
+                                                 'update_admin_mapping',
+                                                 [user1.contract_address, 0, 0]))
+
+    await signer2.send_transaction(admin2, adminAuth.contract_address, 'update_admin_mapping', [user1.contract_address, 0, 0])
+
+    execution_info2 = await adminAuth.get_admin_mapping(user1.contract_address, 0).call()
+    assert execution_info2.result.allowed == 0
+
+    result = await adminAuth.get_current_total_admins().call()
+    assert result.result.res == 2
+
+
+@pytest.mark.asyncio
+async def test_min_admin_threshold_breach(adminAuth_factory):
+    adminAuth, admin1, admin2, user1, user2, user3 = adminAuth_factory
+
+    result = await adminAuth.get_current_total_admins().call()
+    assert result.result.res == 2
+
+    # if removal results in less than min num of admins then transaction will revert
+    await assert_revert(signer1.send_transaction(admin1, adminAuth.contract_address,
+                                                 'update_admin_mapping',
+                                                 [admin2.contract_address, 0, 0]))
 
 
 @pytest.mark.asyncio
 async def test_update_admin_mapping_revoke(adminAuth_factory):
-    adminAuth, admin1, admin2 = adminAuth_factory
+    adminAuth, admin1, admin2, user1, user2, user3 = adminAuth_factory
 
-    await signer1.send_transaction(admin1, adminAuth.contract_address, 'update_admin_mapping', [pytest.user3.contract_address, 1, 1])
+    await signer1.send_transaction(admin1, adminAuth.contract_address, 'update_admin_mapping', [user3.contract_address, 1, 1])
 
-    execution_info1 = await adminAuth.get_admin_mapping(pytest.user3.contract_address, 1).call()
+    execution_info1 = await adminAuth.get_admin_mapping(user3.contract_address, 1).call()
     assert execution_info1.result.allowed == 1
 
-    await signer2.send_transaction(admin2, adminAuth.contract_address, 'update_admin_mapping', [pytest.user3.contract_address, 1, 0])
+    await signer2.send_transaction(admin2, adminAuth.contract_address, 'update_admin_mapping', [user3.contract_address, 1, 0])
 
-    execution_info2 = await adminAuth.get_admin_mapping(pytest.user3.contract_address, 1).call()
+    execution_info2 = await adminAuth.get_admin_mapping(user3.contract_address, 1).call()
     assert execution_info2.result.allowed == 0
+
+
+@pytest.mark.asyncio
+async def test_readd_admin_mapping_admin(adminAuth_factory):
+    adminAuth, admin1, admin2, user1, user2, user3 = adminAuth_factory
+
+    # re-adding a removed admin will still require 2 approvals to update admin mapping
+    result = await adminAuth.get_current_total_admins().call()
+    assert result.result.res == 2
+    await signer1.send_transaction(admin1, adminAuth.contract_address, 'update_admin_mapping', [user1.contract_address, 0, 1])
+
+    execution_info1 = await adminAuth.get_admin_mapping(user1.contract_address, 0).call()
+    assert execution_info1.result.allowed == 0
+
+    result = await adminAuth.get_current_total_admins().call()
+    assert result.result.res == 2
+
+    await signer2.send_transaction(admin2, adminAuth.contract_address, 'update_admin_mapping', [user1.contract_address, 0, 1])
+
+    execution_info2 = await adminAuth.get_admin_mapping(user1.contract_address, 0).call()
+    assert execution_info2.result.allowed == 1
+
+    result = await adminAuth.get_current_total_admins().call()
+    assert result.result.res == 3
