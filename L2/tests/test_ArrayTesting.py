e921d29030c7e5debd7c87e76e82950729e85d7d
Vishnu Prakash <stanbrooks07@gmail.com>
2022-08-12 11:10:49 +0530

    Removed changes in ArrayTesting.py

--- L2/tests/test_ArrayTesting.py
+++ L2/tests/test_ArrayTesting.py
@@ -3,12 +3,11 @@ import asyncio
 from starkware.starknet.testing.starknet import Starknet
 from starkware.starkware_utils.error_handling import StarkException
 from starkware.starknet.definitions.error_codes import StarknetErrorCode
+
 from utils import Signer, uint, str_to_felt, MAX_UINT256, assert_revert
-from helpers import StarknetService, ContractType
 
 signer1 = Signer(123456789987654321)
 
-L1_dummy_address = 0x01234567899876543210
 
 @pytest.fixture(scope='module')
 def event_loop():
@@ -16,24 +15,25 @@ def event_loop():
 
 
 @pytest.fixture(scope='module')
-async def contract_factory(starknet_service: StarknetService):
-    
-    admin1 = await starknet_service.deploy(ContractType.Account, [
-        signer1.public_key,
-        L1_dummy_address,
-        0,
-        1
-    ])
+async def contract_factory():
+    starknet = await Starknet.empty()
+    admin1 = await starknet.deploy(
+        "contracts/Account.cairo",
+        constructor_calldata=[signer1.public_key, 0]
+    )
 
-    arrayTesting = await starknet_service.deploy(ContractType.ArrayTesting, [])
+    arrayTesting = await starknet.deploy(
+        "contracts/ArrayTesting.cairo",
+        constructor_calldata=[]
+    )
 
     return arrayTesting, admin1
 
 
 @pytest.mark.asyncio
 async def create_positions(contract, account):
-    for i in range(4000):
-        await signer1.send_transaction(account, contract.contract_address, 'add_position', [i+1, (i+1)*10, (i+1)*100, i, i, i, i, i, i, i, i, i, i])
+    for i in range(5):
+        await signer1.send_transaction(account, contract.contract_address, 'add_position', [i+1, (i+1)*10, (i+1)*100])
 
 
 @pytest.mark.asyncio
@@ -42,5 +42,27 @@ async def test_get_admin_mapping(contract_factory):
 
     await create_positions(arrayTesting, admin1)
 
+    # await signer1.send_transaction(admin1, arrayTesting.contract_address, 'add_position', [ 1, 261, 361])
+
+    for i in range(1, 6):
+        position = await arrayTesting.get_position(i).call()
+        print("Index ", i, ": ", position.result.res)
+
+        position_array = await arrayTesting.get_position_array(i-1).call()
+        print("Index ", i, ": ", position_array.result.res)
+
+        assert position.result.res == position_array.result.res
+
+    # Removing Assets
+    await signer1.send_transaction(admin1, arrayTesting.contract_address, 'remove_from_array', [0])
+
+    position_last = await arrayTesting.get_position(5).call()
+    print("After deletion: ", position_last.result.res)
+
+    position_array = await arrayTesting.get_position_array(0).call()
+    print(position_array.result.res)
+
+    assert position_last.result.res == position_array.result.res
+
     array = await arrayTesting.return_array().call()
-    print(len(array.result.array_list))
+    print(array.result)
