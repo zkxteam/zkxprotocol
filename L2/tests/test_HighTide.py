4e14b024ac5e26ecfb2c3371d85e3bed6266fcf1
Swathi Reddy <swathireddy@Swathis-MacBook-Air.local>
2022-10-04 15:45:28 +0530

    ZKX-850 checks events emitted in tests

--- L2/tests/test_HighTide.py
+++ L2/tests/test_HighTide.py
@@ -4,7 +4,7 @@ import time
 from starkware.starknet.testing.starknet import Starknet
 from starkware.starkware_utils.error_handling import StarkException
 from starkware.starknet.definitions.error_codes import StarknetErrorCode
-from utils import Signer, uint, str_to_felt, MAX_UINT256, assert_revert, to64x61, PRIME
+from utils import Signer, uint, str_to_felt, MAX_UINT256, assert_revert, to64x61, PRIME, assert_event_emitted
 from helpers import StarknetService, ContractType, AccountFactory
 from dummy_addresses import L1_dummy_address
 from dummy_signers import signer1, signer2, signer3
@@ -45,8 +45,17 @@ async def test_set_multipliers_unauthorized_user(adminAuth_factory):
 async def test_set_multipliers_authorized_admin(adminAuth_factory):
     adminAuth, hightide, admin1, admin2, user1 = adminAuth_factory
 
-    await signer1.send_transaction(admin1, hightide.contract_address, 'set_multipliers', [
+    set_multipliers_tx = await signer1.send_transaction(admin1, hightide.contract_address, 'set_multipliers', [
         1, 2, 3, 4])
+    
+    assert_event_emitted(
+        set_multipliers_tx,
+        from_address=hightide.contract_address,
+        name="multipliers_for_rewards_added",
+        data=[
+            admin1.contract_address,1,2,3,4
+        ]
+    )
 
     execution_info = await hightide.get_multipliers().call()
     fetched_multipliers = execution_info.result.multipliers
@@ -67,8 +76,17 @@ async def test_set_constants_unauthorized_user(adminAuth_factory):
 async def test_set_constants_authorized_admin(adminAuth_factory):
     adminAuth, hightide, admin1, admin2, user1 = adminAuth_factory
 
-    await signer1.send_transaction(admin1, hightide.contract_address, 'set_constants', [
+    set_constants_tx = await signer1.send_transaction(admin1, hightide.contract_address, 'set_constants', [
         1, 2, 3, 4, 5])
+    
+    assert_event_emitted(
+        set_constants_tx,
+        from_address=hightide.contract_address,
+        name="constants_for_trader_score_added",
+        data=[
+            admin1.contract_address,1,2,3,4,5
+        ]
+    )
 
     execution_info = await hightide.get_constants().call()
     fetched_constants = execution_info.result.constants
@@ -92,8 +110,19 @@ async def test_setup_trading_season_authorized_admin(adminAuth_factory):
     adminAuth, hightide, admin1, admin2, user1 = adminAuth_factory
 
     timestamp = int(time.time())
-    await signer1.send_transaction(admin1, hightide.contract_address, 'setup_trade_season', [
+    trade_season_setup_tx = await signer1.send_transaction(admin1, hightide.contract_address, 'setup_trade_season', [
         str_to_felt("100"), timestamp, to64x61(30)])
+    
+    assert_event_emitted(
+        trade_season_setup_tx,
+        from_address=hightide.contract_address,
+        name="trading_season_set_up",
+        data=[
+            admin1.contract_address,
+            timestamp,
+            to64x61(30)
+        ]
+    )
 
     execution_info = await hightide.get_season(str_to_felt("100")).call()
     fetched_trading_season = execution_info.result.trading_season
@@ -107,6 +136,27 @@ async def test_start_trade_season_unauthorized_user(adminAuth_factory):
 
     await assert_revert( signer3.send_transaction(user1, hightide.contract_address, 'start_trade_season', [
         str_to_felt("100")]))
+
+@pytest.mark.asyncio
+async def test_start_trade_season_authorized_admin(adminAuth_factory):
+    adminAuth, hightide, admin1, admin2, user1 = adminAuth_factory
+
+    start_trade_season_tx = await signer1.send_transaction(admin1, hightide.contract_address, 'start_trade_season', [
+        str_to_felt("100")])
+    
+    assert_event_emitted(
+        start_trade_season_tx,
+        from_address=hightide.contract_address,
+        name="trading_season_started",
+        data=[
+            admin1.contract_address, str_to_felt("100")
+        ]
+    )
+    
+    execution_info = await hightide.get_current_season_id().call()
+    fetched_season_id = execution_info.result.season_id
+
+    assert fetched_season_id == str_to_felt("100")
 
 @pytest.mark.asyncio
 async def test_get_season_with_invalid_season_id(adminAuth_factory):
